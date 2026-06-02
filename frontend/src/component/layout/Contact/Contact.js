6bd08407e8f8871e27de1e8a9fadcb8c836c557b
Your Name <gaurangkapil480@gmail.com>
2026-06-02 10:10:44 +0530

    Update contact email

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -5,8 +5,13 @@ import { Button } from "@material-ui/core";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
-        <Button>Contact: mymailforabhi@gmail.com</Button>
+      <a
+        className="mailBtn"
+        href="mailto:singhshreya1809@gmail.com"
+      >
+        <Button>
+          Contact: singhshreya1809@gmail.com
+        </Button>
       </a>
     </div>
   );
